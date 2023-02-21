94a553844c3ad9b248d5c1b2ce06c229cc14e4f8
7698205509 <115994965+7698205509@users.noreply.github.com>
2023-02-21 18:37:59 +0530

    Add files via upload

---
+++ minno.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int number;
+	printf("enter no.: ");
+	scanf("%d",&number);
+	if(number<10){
+		printf("mimimum no.");
+	}
+	else{
+		printf("eror");
+	}
+	
+	
+	
+}
---
+++ neutral.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter no.: ");
+	scanf("%d",&a);
+	if(a>0){
+		printf("positive");
+	}
+	
+	else  {
+		printf("nuetral");
+	}
+	
+	return 0;
+	
+	
+	
+}
